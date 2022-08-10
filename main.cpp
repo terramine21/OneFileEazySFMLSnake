11e6ef60e1b8b38347a6f35a5cd0bd5386d2a571
terramine21 <78859767+terramine21@users.noreply.github.com>
2022-08-10 20:24:59 +0300

    Update main.cpp
    GLOGAL REWORK

--- main.cpp
+++ main.cpp
@@ -4,94 +4,6 @@
 #include <iostream>
 #include <cassert>
 
-//есть две карты, одна карта - массив с яблоками, змейкой и прочим на доске
-//вторая - прост заполнение пустоты и рисование кадра я вижу следующим образом:
-/*
-	void World::FrameMaker(std::size_t mapSize, vector<Snake>& snake)
-	{
-		vector<vector<Cells::ID>> background = ......
-		...
-	}
-	void World::FrameMaker(vector<vector<Cells::ID>>& frame)
-	{
-	}
-
-*/
-//или пойти по-другому и дать классу знать только свою позицию, но тогда придётся проверять все массивы, и наложение кря..
-
-//Сейчас я хочу пойти по пути, когда я нахожу в стэке клеток клетку и происходит событие и всё через карту.
-//Первоначальная отрисовка кадра - рисуется всё, а потом только те места, которые изменились.
-//Хвост змейки заменятеся клеткой, кушаем яблоко и не заменятеся, по сути отказываюсь от вектора змейки,
-//а голова единственная имеет направление, которое передаётся в функцию передвижения.
-//У каждой клетки есть своё действие, которое активируется при наступлении на неё головой.
-//Cell - просто шалон класса, по которому строится уже всё остальное
-//CellStak - хранит клетки
-//
-
-//namespace Cells
-//{
-//	enum ID
-//	{
-//		Standart, 
-//		Wall,
-//		Apple,
-//		SnakeSegment,
-//		SnakeHead,
-//	};
-//}
-//
-//class CellStack;
-//class Cell
-//{
-//public:
-//	typedef std::unique_ptr<Cell> Ptr;
-//	Cell(CellStack& stack, sf::RenderWindow& window)
-//		: mStack(&stack)
-//		, mWindow(&window)
-//	{
-//	}
-//
-//	//virtual void draw() = 0;
-//	//virtual bool update() = 0;
-//	//virtual bool update(sf::Time dt) = 0;
-//	////virtual bool handleEvent(const sf::Event& event) = 0;
-//	//virtual bool doWork() = 0;
-//
-//private:
-//	CellStack*		  mStack;
-//	sf::RenderWindow* mWindow;
-//};
-//
-//class CellStack
-//{
-//public:
-//	CellStack(sf::RenderWindow& window) : mStack(), mWindow(&window)
-//	{
-//
-//	}
-//
-//	template <typename T>
-//	void registerCell(Cells::ID id)
-//	{
-//		mStack[id] = std::make_unique<T>(*this, *mWindow);
-//	}
-//private:
-//	std::map<Cells::ID ,Cell::Ptr> mStack;
-//	sf::RenderWindow* mWindow;
-//};
-//
-//class SnakeHead : public Cell
-//{
-//public:
-//	SnakeHead(CellStack& stack, sf::RenderWindow& window)
-//		: Cell(stack, window)
-//	{
-//	}
-//private:
-//
-//};
-
-
 
 namespace Cells
 {
@@ -103,115 +15,112 @@ namespace Cells
 		SnakeSegment,
 		SnakeHead,
 	};
+}
 
-	Cells::ID Map[20][20]
+namespace Action
+{
+	enum ID
 	{
-		{Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart},
-		{Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart},
-		{Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart},
-		{Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart},
-		{Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart},
-		{Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart},
-		{Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart},
-		{Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart},
-		{Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart},
-		{Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart},
-		{Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart},
-		{Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart},
-		{Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart},
-		{Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart},
-		{Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart},
-		{Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart},
-		{Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart},
-		{Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart},
-		{Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart},
-		{Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart,Cells::Standart}
+		None,
+		AddSegment,
+		Move,
 	};
 }
 
-class Cell
+
+class Cell //знает куда, как и что рисовать
 {
 public:
 	typedef std::unique_ptr<Cell> Ptr;
-
-	Cell(sf::RenderWindow& window, sf::Color color = sf::Color::Black)
-		: mWindow(window)
-		, mColor(color)
-	{}
-	virtual void draw(sf::Vector2i pos, sf::CircleShape& shape) {}
-	virtual void doSomething() { std::cout << "nothing" << std::endl; }
-
-	sf::RenderWindow& getWindow() { return mWindow; }
-
-	sf::Color mColor;
-private:
-	sf::RenderWindow& mWindow;
-
-};
-
-class Standart : public Cell
-{
-public:
-	Standart(sf::RenderWindow& window, sf::Color color = sf::Color::Blue)
-		: Cell(window, color)
+	
+	struct Context
 	{
+		Context(sf::RenderWindow& window, std::size_t& scale,  std::vector<sf::Texture>& textures, std::vector<sf::Color>& colors) //shapes - вектор с фигурами, я просто буду их двигать и рисовать
+			: window(window)
+			, scale(scale)
+			, textures(textures)
+			, colors(colors)
+		{
+		}
+		Context();
 
+		sf::RenderWindow& window;
+		std::size_t& scale;
+		std::vector<sf::Texture>& textures;
+		std::vector<sf::Color>& colors;
+	};
+
+	Cell(sf::Vector2i pos, Cells::ID id, Context& context)
+		: mContext(context)
+		, mID(id)
+	{
+		mShape.setPosition(sf::Vector2f(pos.x * mContext.scale, pos.y * mContext.scale));
+		mShape.setTexture(&mContext.textures[mID]);
 	}
-		void draw(sf::Vector2i pos, sf::CircleShape & shape)
-		{
-			shape.setPosition(pos.x*shape.getRadius()*2, pos.y*shape.getRadius() * 2);
-			shape.setFillColor(mColor);
-			getWindow().draw(shape);
-		}
-		void doSomething() 
-		{
-			std::cout << "Standart" << std::endl; 
-		}
+	void draw()
+	{
+		mContext.window.draw(mShape);
+	}
+	 Cells::ID returnID() 
+	{
+		 return mID;
+	} //это по сути айди действия, которое обрабатывается в MapManage
+
+	//Context getContext() { return mContext; }
+private:
+	Context& mContext;
+	Cells::ID mID;
+	sf::RectangleShape mShape;
 };
+
 
 class MapManage
 {
 public:
-	MapManage(sf::RenderWindow& window, sf::CircleShape& shape) 
+	MapManage(sf::RenderWindow& window, std::vector<sf::Texture>& textures, std::vector<sf::Color>& colors)
 		: mWindow(window)
-		, mShape(shape)
-	{}
-
-	void firstDrawMap(sf::CircleShape shape)
+		, mTextures(textures)
+		, mColors(colors)
 	{
-		for (std::size_t i = 0; i < 20; i++)
-			for (std::size_t j = 0; j < 20; j++)
-				doSomething(sf::Vector2i(j, i), Cells::Map[j][i]);
+		createMap();
 	}
 
-
-	void doSomething(sf::Vector2i pos, Cells::ID id) 
-	{ 
-		mVector[id]->draw(pos, mShape);
-		mVector[id]->doSomething(); 
-	}
-
-	template <typename T>
-	void registerCell(Cells::ID id)
+	void draw()
 	{
-		mVector[id] = std::make_unique<T>(mWindow);
+		for (std::size_t i = 0; i < 10; i++)
+			for (std::size_t j = 0; j < 10; j++)
+				Map[i][j]->draw();
 	}
+
+	void createMap()
+	{
+		std::vector<Cell*> vec;
+		for (std::size_t i = 0; i < 10; i++)
+			vec.push_back(new Cell(sf::Vector2i(i, 0), Cells::Standart, mContext));
+		for (std::size_t i = 0; i < 10; i++)
+			Map.push_back(vec);
+	}
+
+
 
 private:
-	std::map < Cells::ID , Cell::Ptr > mVector;
 	sf::RenderWindow& mWindow;
-	sf::CircleShape& mShape;
+	std::vector<sf::Texture>& mTextures;
+	std::vector<sf::Color>& mColors;
+	std::vector<std::vector<Cell*>> Map;
+	Cell::Context mContext;
 };
 int main()
 {
 	sf::RenderWindow window(sf::VideoMode(800, 800), "OneFileSFMLSnake");
 
 	sf::CircleShape shape(25.f);
+	sf::RectangleShape rectangle(sf::Vector2f(shape.getRadius() * 2, shape.getRadius() * 2));
 	shape.setFillColor(sf::Color::Red);
 
-	MapManage vec(window, shape);
-	vec.registerCell<Standart>(Cells::Standart);
 
+	MapManage manage(window, textures, colors);
+	manage.createMap();
 
 	while (window.isOpen())
 	{
@@ -226,11 +135,9 @@ int main()
 
 
 
+
 		window.clear();
-
-		vec.firstDrawMap(shape);
-
-		//window.draw(shape);
+		manage.draw();
 		window.display();
 	}
 
