2d50b08ca5dfcba5b447c58bedddc9f54fe6beaa
terramine21 <78859767+terramine21@users.noreply.github.com>
2022-08-12 21:11:50 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,4 +1,6 @@
 #include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+
 #include <vector>
 #include <functional>
 #include <iostream>
@@ -9,6 +11,7 @@ namespace Cells
 {
 	enum ID
 	{
+		None,
 		Standart,
 		Wall,
 		Apple,
@@ -16,24 +19,36 @@ namespace Cells
 		SnakeHead,
 	};
 }
-
+namespace Audios
+{
+	enum ID
+	{
+		eat,
+	};
+}
 namespace Action
 {
 	enum ID
 	{
 		None,
 		AddSegment,
+		SpawnApple,
+		//DeledeApple,
+		MoveApple,
 		StandartMove,
 		MoveUp,
 		MoveDown,
 		MoveLeft,
 		MoveRight,
+		CheckCellAction,
 		GameOver,
+		MoveSnake,
+		PlayEat,
 	};
 
 	enum Move
 	{
-		Up, Down, Left, Right
+		Up, Down, Left, Right, Wait
 	};
 }
 
@@ -67,6 +82,7 @@ private:
 	std::map<Identifier, std::unique_ptr<Resource>> mTextureMap; //уникальный указатель, то есть указатель если два указателя указывает на одно, то остаётся только последне созданный указатель
 };
 typedef ResourceHolder<sf::Texture, Cells::ID> TextureHolder;
+typedef ResourceHolder<sf::SoundBuffer, Audios::ID> SoundHolder;
 
 class Cell //знает куда, как и что рисовать
 {
@@ -75,22 +91,25 @@ public:
 	
 	struct Context
 	{
-		Context(sf::RenderWindow& window, float scale,  TextureHolder& textures)
+		Context(sf::RenderWindow& window, float scale,  TextureHolder& textures, SoundHolder& sound)
 			: window(&window)
 			, scale(scale)
 			, textures(&textures)
+			, sound(&sound)
 		{}
 		Context() {}
 
 		sf::RenderWindow* window;
 		float scale;
 		TextureHolder* textures;
+		SoundHolder* sound;
 	};
 
 	Cell(sf::Vector2i pos, Cells::ID id, Context& context)
 		: mContext(&context)
 		, mID(id)
-		, mShape(sf::Vector2f(1,1))
+		, mShape(sf::Vector2f(1, 1))
+		, addOn(nullptr)
 	{
 		mShape.setScale(sf::Vector2f(mContext->scale, mContext->scale));
 		mShape.setPosition(sf::Vector2f(pos.x * mContext->scale, pos.y * mContext->scale));
@@ -99,6 +118,8 @@ public:
 	void draw()
 	{
 		mContext->window->draw(mShape);
+		if(addOn)
+			addOn->draw();
 	}
 	void reassignment(Cells::ID id) 
 	{
@@ -106,10 +127,13 @@ public:
 		mShape.setTexture(&mContext->textures->get(id));
 	}
 
-	 Cells::ID returnID() 
+	Cells::ID getID()
 	{
-		 return mID;
-	} //это по сути айди действия, которое обрабатывается в MapManage
+		if (mID == Cells::Standart && addOn)
+			return addOn->getID();
+		else
+			return mID;
+	}
 
 	 sf::Vector2i getPos()
 	 {
@@ -120,11 +144,20 @@ public:
 		 mShape.setPosition(sf::Vector2f(pos.x * mShape.getScale().x, pos.y * mShape.getScale().y));
 	 }
 
-	Context* getContext() { return mContext; }
+	Context* getContext() 
+	{
+		return mContext;
+	}
+
+	void setAddOn(Cell* cell) 
+	{
+		addOn = cell; 
+	}
 private:
 	Context* mContext;
-	Cells::ID mID;
 	sf::RectangleShape mShape;
+	Cells::ID mID;
+	Cell* addOn; //надстройка
 };
 
 class MapManager
@@ -149,11 +182,15 @@ public:
 	{
 		Map[pos.y][pos.x]->reassignment(id); 
 	}
-
+	void SetAddOn(sf::Vector2i pos, Cell* cell)
+	{
+		Map[pos.y][pos.x]->setAddOn(cell);
+	}
 	Cell* getCell(sf::Vector2i pos)
 	{
 		return Map[pos.y][pos.x];
 	}
+	std::size_t getSize() { return Map.size(); }
 
 	void create()
 	{
@@ -171,7 +208,6 @@ public:
 			}
 	}
 
-
 private:
 
 private:
@@ -179,13 +215,6 @@ private:
 	std::vector<std::vector<Cell*>>		Map;
 };
 
-class Snake
-{
-public:
-
-private:
-
-};
 
 class Game //Application, потом когда буду добавлять меню изменю
 {
@@ -193,52 +222,86 @@ public:
 	Game()
 		: mWindow(sf::VideoMode(800, 800), "OneFileSFMLSnake")
 		, mTextures()
-		, mContext(mWindow, 25, mTextures) //пока так
+		, mContext(mWindow, 25, mTextures, mSound) //пока так
 		, mapManager(mContext)
 		, direct(Action::Right)
 	{
 		addTexture();
 		mapManager.create();
-
-		mSnake.push_back(new Cell(sf::Vector2i(2, 2), Cells::Wall, mContext));
+		//
+		mSnake.push_back(new Cell(sf::Vector2i(2, 2), Cells::SnakeHead, mContext));
+		mSnake.push_back(new Cell(sf::Vector2i(2, 2), Cells::SnakeSegment, mContext));
+		mSnake.push_back(new Cell(sf::Vector2i(2, 2), Cells::SnakeSegment, mContext));
+		
+		sf::Sound eat(mSound.get(Audios::eat));
+		eat.setVolume(100);
+		//
+		doSomething(Action::SpawnApple);
 	}
 
 	void run()
 	{
+		sf::Clock clock;
+		float speed = 0.5;
 
 		while (mWindow.isOpen())
 		{
+			mWindow.clear();
+			mapManager.draw(mSnake);
+			mWindow.display();
 			sf::Event event;
 			while (mWindow.pollEvent(event))
 			{
+
 				if (event.type == sf::Event::Closed)
 					mWindow.close();
 				if (event.key.code == sf::Keyboard::Escape)
 					mWindow.close();
-				if (event.key.code == sf::Mouse::Left)
+
+				if (event.key.code == sf::Keyboard::A)
+					direct = Action::Left;
+				else if (event.key.code == sf::Keyboard::D)
+					direct = Action::Right;
+				else if (event.key.code == sf::Keyboard::W)
+					direct = Action::Up;
+				else if (event.key.code == sf::Keyboard::S)
+					direct = Action::Down;
+
+
+				//if (clock.getElapsedTime().asSeconds() >= speed)
+				{
 					doSomething(Action::StandartMove);
+
+					mWindow.clear();
+					mapManager.draw(mSnake);
+					mWindow.display();
+
+					clock.restart();
+				}
 			}
 
-			mWindow.clear();
-			mapManager.draw(mSnake);
-			mWindow.display();
 		}
 	}
 
 private:
-	void cellEffect(Cells::ID id)
+	void cellEffect(Cell* cell)
 	{
-		Action::ID a = Action::None;
-		if (id == Cells::Standart)
-			a = Action::None;
-		else if (id == Cells::Wall)
-			a = Action::GameOver;
-		else if (id == Cells::Apple)
-			a = Action::AddSegment;
-		else if (id == Cells::SnakeSegment)
-			a = Action::GameOver;
+		Cells::ID id = cell->getID();
 
-			doSomething(a);
+		if (id == Cells::Standart)
+			doSomething(Action::None);
+		else if (id == Cells::Wall)
+			doSomething(Action::GameOver);
+		else if (id == Cells::Apple)
+		{
+			doSomething(Action::AddSegment);
+			doSomething(Action::MoveApple);
+			doSomething(Action::PlayEat);
+		}
+		else if (id == Cells::SnakeSegment)
+			doSomething(Action::GameOver);
+
+
 	}
 	void doSomething(Action::ID action)
 	{
@@ -247,45 +310,58 @@ private:
 		case Action::		None:
 			break;
 		case Action::		AddSegment:
-			//return [this]() {};
+			mSnake.push_back(new Cell(oldPos, Cells::SnakeSegment, mContext));
 			break;
 		case Action::		StandartMove:
 		{
-			sf::Vector2i oldPos = mSnake[0]->getPos();
-
+			sf::Vector2i t(0, 0);
 			if (direct == Action::Right)
-				mSnake[0]->setPos(sf::Vector2i(mSnake[0]->getPos().x + 1, mSnake[0]->getPos().y));
+				t.x + 1;
 			else if (direct == Action::Down)
-				mSnake[0]->setPos(sf::Vector2i(mSnake[0]->getPos().x, mSnake[0]->getPos().y + 1));
+				t.y + 1;
 			else if (direct == Action::Left)
-				mSnake[0]->setPos(sf::Vector2i(mSnake[0]->getPos().x - 1, mSnake[0]->getPos().y));
+				t.x - 1;
 			else if (direct == Action::Up)
-				mSnake[0]->setPos(sf::Vector2i(mSnake[0]->getPos().x, mSnake[0]->getPos().y - 1));
-
-			int k = mSnake.size() - 1;
-			for (size_t i = 1; i < mSnake.size(); i++)
-			{
-				sf::Vector2i t = mSnake[i]->getPos();
-				mSnake[i]->getPos() = oldPos;
-				oldPos = t;
-			}
+				t.y - 1;
+			moveSnake(t);
 		}
 			break;
-		case Action::		MoveUp:
-			//return [this]()
-			break;
-		case Action::		MoveDown:
-			//return [this]() {};
-			break;
-		case Action::		MoveLeft:
-			//return [this]() {};
-			break;
-		case Action::		MoveRight:
-			//return [this]() {};
-			break;
 		case Action::		GameOver:
-			//return [this]() {};
-			break;
+		{
+			std::cout << "YOU LOSE" << std::endl; //временно
+			exit(25);
+		}
+		break;
+		case Action::		SpawnApple:
+		{
+			std::srand(time(0));
+			mApple = new Cell(
+				sf::Vector2i(1 + rand() % (mapManager.getSize() - 2), 1 + rand() % (mapManager.getSize() - 2)),
+				Cells::Apple, mContext);
+			mapManager.SetAddOn(mApple->getPos(), mApple);
+		}
+		break;
+		case Action::MoveApple:
+		{
+			std::srand(time(0));
+			sf::Vector2i apos;
+			
+			while (true)
+			{
+				apos = sf::Vector2i(rand() % (mapManager.getSize()), rand() % (mapManager.getSize()));
+				if (mapManager.getCell(apos)->getID() == Cells::Standart)
+				{
+					mApple->setPos(apos);
+					mapManager.SetAddOn(mApple->getPos(), mApple);
+					break;
+				}
+
+			}
+
+		}
+		break;
+
+		break;
 		default:
 			break;
 		}
@@ -295,28 +371,62 @@ private:
 		mTextures.load(Cells::Standart, "Textures/Standart.png"); //потом отдельно вынесу как скрины кадра, если не будет лень
 		mTextures.load(Cells::Wall, "Textures/Wall.png");
 		mTextures.load(Cells::Apple, "Textures/Apple.png");
+		mTextures.load(Cells::SnakeHead, "Textures/SnakeHead.png");
+		mTextures.load(Cells::SnakeSegment, "Textures/SnakeSegment.png");
+		//
+		mSound.load(Audios::eat, "Textures/hrum.wav");
+		//
 	}
 	typedef void воид;
 	воид кек()
 	{
 		std::cout << "kek" << std::endl;
 	}
+
+	void moveSnake(sf::Vector2i move)
+	{
+		oldPos = mSnake[0]->getPos();
+
+		int k = mSnake.size() - 1;
+		for (size_t i = 1; i < mSnake.size(); i++)
+		{
+			sf::Vector2i t = mSnake[i]->getPos();
+			mapManager.SetAddOn(oldPos, mSnake[i]);
+			mSnake[i]->setPos(oldPos);
+			oldPos = t;
+		}
+		mapManager.SetAddOn(oldPos, nullptr);
+
+		cellEffect(mapManager.getCell(mSnake[0]->getPos()));
+		mapManager.SetAddOn(mSnake[0]->getPos(), mSnake[0]);
+	}
+
 private:
 	MapManager mapManager; //в нём же и карта
 	// избавиться от следующего
 	std::vector<Cell*> mSnake;
 	Action::Move direct;
+	sf::Vector2i oldPos;
+	Cell* mApple;
 	//
 	sf::RenderWindow mWindow;
 	TextureHolder mTextures;
+	SoundHolder mSound;
 	Cell::Context mContext;
 };
 
 int main()
 {
-	Game game;
-	game.run();
-	
+	try
+	{
+		Game game;
+		game.run();
+	}
+	catch (const std::exception& e)
+	{
+		std::cout << "\nEXCEPTION: " << e.what() << std::endl;
+	}
+
 
 	return 0;
 }
